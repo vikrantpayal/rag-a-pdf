7bc8452932c6d0b80160a193dbdb5b65b0db1fa1
Vikrant Payal <vikrantpayal@gmail.com>
2024-06-20 10:44:20 +0000

    .

--- rag-it.py
+++ rag-it.py
@@ -1,10 +1,13 @@
 from torch import cuda 
+from langchain_community.llms import HuggingFaceEndpoint # NEXT how do I import HF endpoint?
+from langchain.prompts import PromptTemplate
+import os
+from getpass import getpass
 
 device      = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu' 
 device
 
-from langchain_community.llms import HuggingFaceEndpoint # NEXT how do I import HF endpoint?
-from langchain.prompts import PromptTemplate
+hfapi_key = getpass("Enter you HuggingFace access token:")
+os.environ["HF_TOKEN"] = hfapi_key
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = hfapi_key
 
-import os
-from getpass import getpass
